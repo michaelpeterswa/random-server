71ecd89f51adf2c036c5650c6516ca954c02f549
Michael Peters Jr <michael@michaelpeterswa.com>
2025-08-08 16:24:48 -0700

    fix: pb-demo branch

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -75,21 +75,38 @@ func (h *HandlersClient) CatchAllHandler(w http.ResponseWriter, r *http.Request)
 		randomStatusCode := errorCodes[rand.Intn(len(errorCodes))]
 
 		errorDetails := []string{
-			"failed to process request",
-			"uh oh, something went wrong",
-			"unexpected error occurred",
-			"something went wrong",
-			"internal server error",
+			"Event postback is disabled due to Facebook re-engagement being enabled",
+			`PostbackTypeNotSupported: Integration doesn't support this Postback type ("session"). Please contact your Kochava Account Management team.`,
+			"",
+			`{"error":{"message":"(#100) At least one of the parameter 'attribution', 'advertiser_id', 'anon_id', 'page_scoped_user_id', 'user_id_type' or 'ud' is required for the 'custom_app_e`,
+			`{"error":{"message":"(#4) Application request limit reached","type":"OAuthException","is_transient":true,"code":4,"`,
+			`{"status":400,"error":"Bad Request","errors":[{"codes":["typeMismatch.postBackBean.ctawindow","typeMismatch.ctawindow","typeMismatch.java`,
+			"Empty device id.",
+			"Error: Doubleclick only supports adid and idfa identifiers, neither found.",
+			`{"num_events_processed":1,"num_events_received":1,"events":[{"status":"processed","error_message":"","warning_message":""}]}`,
+			"Error: Is Not Allowed Network, Apple Ads",
+			`PostbackTypeNotSupported: Integration doesn't support this Postback type ("click"). Please contact your Kochava Account Management team.`,
+			"Missing delivery URL",
+			`"EventTypeNotSupported: Integration doesn't support the Event type: "". Please contact your Kochava Account Management team."`,
+			"Error: Invalid postback type: event",
+			`{"errors":["no IDFA, GAID, or GUM data found"],"warnings":[]}`,
+			`{"statusCode":422,"success":false}`,
+			"Error: getaddrinfo ENOTFOUND odm-postback.pinsightmedia.com odm-postback.pinsightmedia.com:443",
+			`{"errors":["The request doesn't contain any event"],"warnings":[]}`,
+			"user_id Decryption Failed",
 		}
 
 		randomDetail := errorDetails[rand.Intn(len(errorDetails))]
 
-		rfc9457.NewRFC9457(
-			rfc9457.WithStatus(randomStatusCode),
-			rfc9457.WithDetail(randomDetail),
-			rfc9457.WithTitle("server error"),
-			rfc9457.WithInstance(r.URL.Path),
-		).ServeHTTP(w, r)
+		w.WriteHeader(randomStatusCode)
+		_, _ = w.Write([]byte(randomDetail))
+
+		// rfc9457.NewRFC9457(
+		// 	rfc9457.WithStatus(randomStatusCode),
+		// 	rfc9457.WithDetail(randomDetail),
+		// 	rfc9457.WithTitle("server error"),
+		// 	rfc9457.WithInstance(r.URL.Path),
+		// ).ServeHTTP(w, r)
 		return
 	}
 
